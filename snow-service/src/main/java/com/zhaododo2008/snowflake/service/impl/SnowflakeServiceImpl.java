a26ac21ae0c4eb595f55490189c04d743bf93307
zhaojun <zhaododo@163.com>
2016-12-23 18:23:07 +0800

    修复日志

--- snow-service/src/main/java/com/zhaododo2008/snowflake/service/impl/SnowflakeServiceImpl.java
+++ snow-service/src/main/java/com/zhaododo2008/snowflake/service/impl/SnowflakeServiceImpl.java
@@ -1,5 +1,7 @@
 package com.zhaododo2008.snowflake.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.zhaododo2008.snowflake.service.ISnowflakeService;
@@ -8,11 +10,15 @@ import com.zhaododo2008.snowflake.service.IdWorker;
 @Service
 public class SnowflakeServiceImpl implements ISnowflakeService {
 
+  public static final Logger logger = LoggerFactory.getLogger(SnowflakeServiceImpl.class);
+
   private final IdWorker idWorker = new IdWorker(0,1);
   
   @Override
   public long getSequence() {
-    return idWorker.nextId();
+    long sequence = idWorker.nextId();
+    logger.info( "getSequence {}",sequence );
+    return sequence;
   }
 
 }
